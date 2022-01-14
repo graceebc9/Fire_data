74a7dfc6ecd3bc56555aedfb03a52c9efbe1fb41
graceebc9 <91670329+graceebc9@users.noreply.github.com>
2022-01-14 13:36:21 +0000

    add create modis stack file

---
+++ create_modis_stack.py
@@ -0,0 +1,177 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#Combine the MODIS data into one time block 
+
+
+# In[2]:
+
+
+import rioxarray as rxr
+from rioxarray import merge
+from rasterio.plot import show 
+import xarray as xr 
+import glob
+
+import os
+import datetime
+import fiona
+
+
+# In[3]:
+
+
+import routines
+from routines import extract_vertices, plot, crop_data_spatially 
+
+
+# In[ ]:
+
+
+#file path for jasmin 
+path = '/neodc/esacci/fire/data/burned_area/MODIS/pixel/v5.1/compressed'
+
+
+# In[ ]:
+
+
+#Access all files and unzip 
+year_list = range(2001,2003)
+months = ['01', '02', '03', '04', '05', '06', '07', '08' ,'09' ,'10', '11', '12']
+output_path = '/home/users/graceebc/MODIS/'
+
+
+# In[ ]:
+
+
+files = ['/neodc/esacci/fire/data/burned_area/MODIS/pixel/v5.1/compressed/{0}/{0}{1}01-ESACCI-L3S_FIRE-BA-MODIS-AREA_3-fv5.1.tar.gz'.format(year, month) for year in year_list for month in months]
+
+for file_name in files:
+    #check if file there
+    # open file
+  
+    file = tarfile.open(file_name)
+
+    # extracting file
+    file.extractall(output_path)
+
+    file.close()
+
+
+# In[4]:
+
+
+
+
+JD_files = sorted(glob.glob('/home/users/graceebc/MODIS/*JD.tif'))
+CL_files = sorted(glob.glob('/home/users/graceebc/MODIS/*CL.tif'))
+
+whole_map = '/home/users/gracebc/Fire_data/whole_map.shp'
+
+
+# In[4]:
+
+
+
+n=4
+chunked_files = [JD_files[i:i + n] for i in range(0, len(JD_files), n)]
+
+
+# In[ ]:
+
+
+final = [] 
+
+for i in range(len(chunked_files)):
+    elements =[]
+    for file in chunked_files[i]:
+        #create date array to add to dataset 
+        date_str = file[12:20]
+        year, month, day  = int(date_str[:4]), int(date_str[4:6]), int(date_str[6:8])
+        date = datetime.datetime(year, month, day)
+        time_da = xr.Dataset({"date": date})
+
+        #open dataset and add time , then store in elements 
+        base = rxr.open_rasterio(file)
+        base_days_clipped = crop_data_spatially(base, whole_map, -2) # set to non burnable 
+        ds = base_days_clipped
+        dst = ds.expand_dims(time=time_da.to_array()) 
+        elements.append(dst)
+    stack = xr.concat(elements, dim='time')
+    print('Done!')
+    final.append(stack)
+    
+
+
+
+# In[7]:
+
+
+stack
+
+
+# 
+
+# In[ ]:
+
+
+
+stack = xr.concat(final, dim='time')
+
+
+# In[8]:
+
+
+
+elements =[]
+
+for file in chunked_files[0]:
+    date_str = file[12:20]
+    year = int(date_str[:4])
+    month = int(date_str[4:6])
+    day = int(date_str[6:8])
+    date = datetime.datetime(year, month, day)
+    time_da = xr.Dataset({"date": date})
+    
+    base = xr.open_dataset(file)
+    ds = base.to_array()
+    dst = ds.expand_dims(time=time_da.to_array()) #errors here
+    elements.append(dst)
+
+ 
+
+
+# In[15]:
+
+
+stack = xr.concat(elements, dim='time')
+stack
+
+
+# In[14]:
+
+
+stack1 = stack.to_dataset(name = 'test_123')
+stack1
+
+
+# In[ ]:
+
+
+
+
+
+# In[17]:
+
+
+stack.to_netcdf('test.nc')
+
+
+# In[ ]:
+
+
+
+
